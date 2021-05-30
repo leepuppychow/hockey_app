35ed405691b9e6e7692fdcd5b36dfc5520c19c3e
leepuppychow <leepuppychow@gmail.com>
2021-05-29 22:39:52 -0600

    Updates relationship on Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  belongs_to :season
   belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
+  has_many :goals
 end
