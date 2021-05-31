63d09c1f02ecd6585c1d46bd5f545cd77ad8cc4a
leepuppychow <leepuppychow@gmail.com>
2021-05-31 11:15:05 -0600

    Creates POST /api/v1/leagues endpoint

--- app/controllers/api/v1/leagues_controller.rb
+++ app/controllers/api/v1/leagues_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::LeaguesController < ApiController
   def index
     render json: League.all, status: 200
   end
-end
+
+  def create
+    league = League.new(league_params)
+    if league.save
+      render json: league, status: 201
+    else
+      render json: {:error => "Error creating new hockey league"}, status: 400
+    end
+  end
+
+  private
+
+    def league_params
+      params.permit(:name)
+    end
+end 
