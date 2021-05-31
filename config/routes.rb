c437ec6f209b04edaaff3a22eb49b5785fb589aa
leepuppychow <leepuppychow@gmail.com>
2021-05-31 17:59:48 -0600

    Adds nested routes for league's seasons

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :leagues
+      resources :leagues do
+        resources :seasons, only: [:create, :update, :destroy]
+      end
     end
   end
+
 end
