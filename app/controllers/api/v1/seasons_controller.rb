816b6280991e43234410128371f8210afe16b3d2
leepuppychow <leepuppychow@gmail.com>
2021-05-31 18:23:36 -0600

    Completes logic in SeasonsController to update a season record

--- app/controllers/api/v1/seasons_controller.rb
+++ app/controllers/api/v1/seasons_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SeasonsController < ApiController
   before_action :find_league
+  before_action :find_season, except: [:create]
 
   def create
     season = Season.new(season_params)
@@ -10,11 +11,30 @@ class Api::V1::SeasonsController < ApiController
       render json: {:error => "Error creating new season"}, status: 400
     end
   end
+  
+  def update
+    if @league.seasons.include?(@season)
+      @season.update(season_params)
+      render json: @season, status: 200
+    else
+      render json: {:error => "Error updating season"}, status: 400
+    end
+  end
 
   private
 
     def find_league
-      @league ||= League.find(params[:league_id])
+      @league ||= League.find_by(id: params[:league_id])
+      if not @league
+        render json: {:error => "Cannot find league #{params[:league_id]}"}, status: 404
+      end
+    end
+
+    def find_season
+      @season ||= Season.find_by(id: params[:id])
+      if not @season
+        render json: {:error => "Cannot find season #{params[:id]}"}, status: 404
+      end
     end
 
     def season_params
